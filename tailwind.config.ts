0b4ddb2867f96d37df2596731047df85effdc2bf
Hanny Tran <cheershanny@gmail.com>
2024-03-30 13:01:42 +0000

    delete default page and set theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["lemonade", "light", "dark"],
+  },
 };
 export default config;
